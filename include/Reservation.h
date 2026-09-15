6996bd9a40397bd1b9fb31405328cd35f20de4ad
Will <WilliamHenryfj@gmail.com>
2026-09-14 19:54:20 -0500

    Removed Resources; fixed studentID camel naming; changed spacing in cpp;  implemented linked list functionality.

--- include/Reservation.h
+++ include/Reservation.h
@@ -3,23 +3,40 @@
 class Reservation {
 
     public:
+    //pointer
+
+    Reservation* next;
+
     //Constructors
-    Reservation();
-    Reservation(std::string resID, std::string stuID, std::string name, std::string rescID, std::string d);
+    Reservation()
+    {
+        this -> reservationID = "";
+        this -> studentID = "";
+        this -> studentName = "";
+        this -> date = "";
+        this -> next = NULL;
+    }
+    Reservation(std::string resID, std::string stuID, std::string name, std::string d)
+    {
+        this -> reservationID = resID;
+        this -> studentID = stuID;
+        this -> studentName = name;
+        this -> date = d;
+        this -> next = NULL;
+    };
+
     //Getters and Setters
 
     std::string getReservationID();
-    std::string getstudentID();
+    std::string getStudentID();
     std::string getStudentName();
-    std::string getResourceID();
     std::string getDate();
     void setReservationID(std::string resID);
     void setStudentID(std::string stuID);
     void setStudentName(std::string name);
-    void setResourceID(std::string rescID);
     void setDate(std::string d);
 
-    //Display Resource Information to the console
+    //Display Reservation Information to the console
     void DisplayReservationInfo();
 
     private:
@@ -27,6 +44,5 @@ class Reservation {
     std::string reservationID; 
     std::string studentID; 
     std::string studentName;
-    std::string resourceID;
     std::string date;
 };
--- src/Reservation.cpp
+++ src/Reservation.cpp
@@ -5,67 +5,35 @@ Reservation::Reservation()
     reservationID = "";
     studentID = "";
     studentName = "";
-    resourceID = "";
     date = "";
+    next = NULL;
 };
-Reservation::Reservation(std::string resID, std::string stuID, std::string name, std::string rescID, std::string d)
+Reservation::Reservation(std::string resID, std::string stuID, std::string name, std::string d)
 {
     reservationID = resID;
     studentID = stuID;
     studentName = name;
-    resourceID = rescID;
     date = d;
+    next = NULL;
 };
 //Getters and Setters  
 
-std::string Reservation::getReservationID() 
-{
-    return reservationID;
-}
-std::string Reservation::getStudentID() 
-{
-return studentID;
-}
-std::string Reservation::getStudentName() 
-{
-    return studentName;
-}
-std::string Reservation::getResourceID() 
-{
-return resourceID;
-}
-std::string Reservation::getDate() 
-{
-    return date;
-}
+std::string Reservation::getReservationID() {return reservationID;}
+std::string Reservation::getStudentID() {return studentID;}
+std::string Reservation::getStudentName() {return studentName;}
+std::string Reservation::getDate() {return date;}
 
-void Reservation::setReservationID(std::string resID) 
-{
-    reservationID = resID;
-}
-void Reservation::setStudentID(std::string stuID) 
-{
-    studentID = stuID;
-}
-void Reservation::setStudentName(std::string name) 
-{
-    studentName = name;
-}
-void Reservation::setResourceID(std::string rescID) 
-{
-    resourceID = rescID;
-}
-void Reservation::setDate(std::string d) 
-{
-    date = d;
-}
-//Display Resource Information to the console
+void Reservation::setReservationID(std::string resID) {reservationID = resID;}
+void Reservation::setStudentID(std::string stuID) {studentID = stuID;}
+void Reservation::setStudentName(std::string name) {studentName = name;}
+void Reservation::setDate(std::string d) {date = d;}
+
+//Display Reservation Information to the console
 void Reservation::DisplayReservationInfo()
 {
     std::cout << "Reservation ID: " << reservationID << std::endl;
     std::cout << "Student ID: " << studentID << std::endl;
     std::cout << "Student Name: " << studentName << std::endl;
-    std::cout << "Resource ID : " << resourceID << std:: endl;
     std::cout << "Listed Date : " << date << std::endl;
 }
 
